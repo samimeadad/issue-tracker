1645c1cbb1c0e86315bab6e12c9d26fd8e906f7f
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-09-12 16:10:31 +0600

    debug Done Alhamdulillah

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function submitIssue ( e ) {
   const description = getInputValue( 'issueDescription' );
   const severity = getInputValue( 'issueSeverity' );
   const assignedTo = getInputValue( 'issueAssignedTo' );
-  const id = Math.floor( Math.random() * 100000000 ) + '';
+  const id = Math.floor( Math.random() * 100000000 );
   const status = 'Open';
 
   const issue = {id, description, severity, assignedTo, status};
@@ -23,6 +23,8 @@ function submitIssue ( e ) {
 
 const closeIssue = id => {
   const issues = JSON.parse( localStorage.getItem( 'issues' ) );
+  console.log( issues[ 0 ].id, typeof issues[ 0 ].id );
+  console.log( id, typeof id );
   const currentIssue = issues.find( issue => issue.id === id );
   currentIssue.status = 'Closed';
   localStorage.setItem( 'issues', JSON.stringify( issues ) );
@@ -33,6 +35,7 @@ const deleteIssue = id => {
   const issues = JSON.parse( localStorage.getItem( 'issues' ) );
   const remainingIssues = issues.filter( issue => issue.id !== id )
   localStorage.setItem( 'issues', JSON.stringify( remainingIssues ) );
+  fetchIssues();
 }
 
 const fetchIssues = () => {
@@ -49,7 +52,7 @@ const fetchIssues = () => {
                               <h3> ${ description } </h3>
                               <p><span class="glyphicon glyphicon-time"></span> ${ severity }</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${ assignedTo }</p>
-                              <a href="#" onclick="setStatusClosed(${ id })" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${ id })" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${ id })" class="btn btn-danger">Delete</a>
                               </div>`;
   }
